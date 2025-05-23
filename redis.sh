357114e8b7c788fec1725cd1c106515a9dbe154c
jay-rapolu <jay.rapolu@outlook.com>
2025-05-23 20:57:55 +0530

    updated redis protected mode

--- redis.sh
+++ redis.sh
@@ -34,7 +34,7 @@ dnf module enable redis:7 -y &>> $LOG_FILE
 VALIDATE $? "Enabling default redis module"
 dnf install redis -y &>> $LOG_FILE
 VALIDATE $? "Installing redis module"
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected mode/ c protected-mode no' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "updating redis configuration"
 systemctl enable redis &>> $LOG_FILE
 VALIDATE $? "enabling redis module"
