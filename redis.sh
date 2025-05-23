6ce8ad933ff8edcf7f353bf13112c23a798899a9
jay-rapolu <jay.rapolu@outlook.com>
2025-05-23 20:50:58 +0530

    redis setup script file

---
+++ redis.sh
@@ -0,0 +1,42 @@
+#!/bin/bash 
+
+USER_ID=$(id -u)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_DIR="/var/log/roboshop-logs"
+LOG_FILE="$SCRIPT_DIR/$SCRIPT_NAME.log"
+
+if [ $USER_ID -ne 0 ]
+then
+    echo "Please run the script as root user or admin user."
+    exit 1
+else
+    echo "Running script as root user"
+    mkdir -p $SCRIPT_DIR
+fi
+
+echo "##########################################" &>> $LOG_FILE
+echo "Script Started executing at '"$(date)"'" | tee -a $LOG_FILE
+echo "##########################################" &>> $LOG_FILE
+
+VALIDATE () {
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is failed:: Exiting the Script" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo "$2 is Success" | tee -a $LOG_FILE
+    fi
+}
+
+dnf module disable redis -y
+VALIDATE $? "Disabling default redis module"
+dnf module enable redis:7 -y
+VALIDATE $? "Enabling default redis module"
+dnf install redis -y 
+VALIDATE $? "Installing redis module"
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected mode/ c protected-mode no/' /etc/redis/redis.conf
+VALIDATE $? "updating redis configuration"
+systemctl enable redis 
+VALIDATE $? "enabling redis module"
+systemctl start redis 
+VALIDATE $? "starting redis module"
