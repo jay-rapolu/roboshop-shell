0b7ed3d891325c5c5c4c3c7681be046b83ea3053
jay-rapolu <jay.rapolu@outlook.com>
2025-05-23 21:03:37 +0530

    updated redis version check

--- redis.sh
+++ redis.sh
@@ -28,15 +28,27 @@ VALIDATE () {
     fi
 }
 
-dnf module disable redis -y &>> $LOG_FILE
-VALIDATE $? "Disabling default redis module"
-dnf module enable redis:7 -y &>> $LOG_FILE
-VALIDATE $? "Enabling default redis module"
-dnf install redis -y &>> $LOG_FILE
-VALIDATE $? "Installing redis module"
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-VALIDATE $? "updating redis configuration"
-systemctl enable redis &>> $LOG_FILE
-VALIDATE $? "enabling redis module"
-systemctl start redis 
-VALIDATE $? "starting redis module"
+redis-server -v
+if [ $? -eq 0 ]
+then
+    echo "redis is already configured.. skipping"
+    exit 1
+else
+    dnf module disable redis -y &>> $LOG_FILE
+    VALIDATE $? "Disabling default redis module"
+    
+    dnf module enable redis:7 -y &>> $LOG_FILE
+    VALIDATE $? "Enabling default redis module"
+    
+    dnf install redis -y &>> $LOG_FILE
+    VALIDATE $? "Installing redis module"
+    
+    sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+    VALIDATE $? "updating redis configuration"
+    
+    systemctl enable redis &>> $LOG_FILE
+    VALIDATE $? "enabling redis module"
+    
+    systemctl start redis 
+    VALIDATE $? "starting redis module"
+fi
