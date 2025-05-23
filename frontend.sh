d156de9c6c78e7141865827e2cdad6d7da5c7af1
jay-rapolu <jay.rapolu@outlook.com>
2025-05-23 22:42:38 +0530

    frontend script updated

--- frontend.sh
+++ frontend.sh
@@ -29,31 +29,31 @@ VALIDATE () {
     fi
 }
 
-dnf module disable nginx -y
+dnf module disable nginx -y &>> $LOG_FILE
 VALIDATE $? "disabling default nginx"
 
-dnf module enable nginx:1.24 -y
+dnf module enable nginx:1.24 -y &>> $LOG_FILE
 VALIDATE $? "disabling nginx version 1.24"
 
-dnf install nginx -y
+dnf install nginx -y &>> $LOG_FILE
 VALIDATE $? "Installing Nginx"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>> $LOG_FILE
 VALIDATE $? "downloading source code"
 
 cd /usr/share/nginx/html
 rm -rf *
-unzip /tmp/frontend
+unzip /tmp/frontend &>> $LOG_FILE
 rm -rf /tmp/frontend
 VALIDATE $? "deploying source code"
 
-systemctl enable nginx
+systemctl enable nginx &>> $LOG_FILE
 VALIDATE $? "Enabling Nginx"
 
 systemctl start nginx
 VALIDATE $? "Starting Nginx"
 
-cp $SCRIPT_PATH/nginx.conf /etc/nginx/nginx.conf
+cp $SCRIPT_PATH/nginx.conf /etc/nginx/nginx.conf 
 VALIDATE $? "Updating Nginx configuration file"
 
 systemctl restart nginx
