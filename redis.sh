ccfd52ed384ee28bbbf595feb277443c2012185b
jay-rapolu <jay.rapolu@outlook.com>
2025-05-23 20:51:46 +0530

    updated redis setup script file

--- redis.sh
+++ redis.sh
@@ -28,15 +28,15 @@ VALIDATE () {
     fi
 }
 
-dnf module disable redis -y
+dnf module disable redis -y &>> $LOG_FILE
 VALIDATE $? "Disabling default redis module"
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y &>> $LOG_FILE
 VALIDATE $? "Enabling default redis module"
-dnf install redis -y 
+dnf install redis -y &>> $LOG_FILE
 VALIDATE $? "Installing redis module"
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected mode/ c protected-mode no/' /etc/redis/redis.conf
 VALIDATE $? "updating redis configuration"
-systemctl enable redis 
+systemctl enable redis &>> $LOG_FILE
 VALIDATE $? "enabling redis module"
 systemctl start redis 
 VALIDATE $? "starting redis module"
